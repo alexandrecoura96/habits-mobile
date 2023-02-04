b9b6cb3b2c3356eb1c5de9bd771a1b77110dab5e
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-04 15:26:23 -0300

    chore: tailwind.config improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        background: "#09090a",
+        background: "#09090A",
       },
       fontFamily: {
         regular: "Inter_400Regular",
