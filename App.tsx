38dce7246cc3bdb374e423b14dbc97b463d5bfc3
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-04 15:02:10 -0300

    chore: App: refinements

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
 import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,10 +22,8 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <View>
+      <Home />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
@@ -33,16 +32,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090A",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#fff",
-    fontFamily: "Inter_800ExtraBold",
-  },
-});
