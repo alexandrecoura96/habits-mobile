c5dc0a93d274d726d6bbe423079006146c11d596
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-07 17:37:06 -0300

    chore: Home: improvements

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
@@ -12,6 +13,8 @@ const ammountOfDaysToFill =
   minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-black px-8 pt-16">
       <Header />
@@ -34,7 +37,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toString()} />
+            <HabitDay
+              key={date.toString()}
+              onPress={() => navigate("Habit", { date: date.toISOString() })}
+            />
           ))}
           {ammountOfDaysToFill > 0 &&
             Array.from({
