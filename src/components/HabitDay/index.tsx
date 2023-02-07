020cd0a53c39109209fb37acad63bacbb2942ceb
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-07 17:36:53 -0300

    chore: HabitDay: improvements

--- src/components/HabitDay/index.tsx
+++ src/components/HabitDay/index.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Dimensions } from "react-native";
 
 const WEEK_DAYS = 7;
@@ -8,12 +8,15 @@ export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
   Dimensions.get("screen").width / WEEK_DAYS - (SCREEN_HORIZONTAL_PADDING + 5);
 
-export function HabitDay() {
+interface IHabitDay extends TouchableOpacityProps {}
+
+export function HabitDay({ ...props }: IHabitDay) {
   return (
     <TouchableOpacity
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
       activeOpacity={0.7}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
+      {...props}
     />
   );
 }
