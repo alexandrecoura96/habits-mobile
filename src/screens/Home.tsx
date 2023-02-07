138e53b7a64e9ec92ca7b1b2cbe049ab8d14dd90
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-07 12:29:38 -0300

    chore: Home: generateDatesFromYearBeginning implemented

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { Text, View } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const datesFromYearStart = generateDatesFromYearBeginning();
+const minimumSummaryDatesSizes = 18 * 5;
+const ammountOfDaysToFill =
+  minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -22,7 +28,21 @@ export function Home() {
           );
         })}
       </View>
-      <HabitDay />
+      <View className="flex-row flex-wrap">
+        {datesFromYearStart.map((date) => (
+          <HabitDay key={date.toString()} />
+        ))}
+        {ammountOfDaysToFill > 0 &&
+          Array.from({
+            length: ammountOfDaysToFill,
+          }).map((_, index) => (
+            <View
+              key={index}
+              className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+              style={{ width: DAY_SIZE, height: DAY_SIZE }}
+            />
+          ))}
+      </View>
     </View>
   );
 }
