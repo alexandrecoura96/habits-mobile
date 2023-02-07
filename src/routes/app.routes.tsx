f505e060ec9e8f88575867dfc2c280a18b03cccc
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-07 17:01:19 -0300

    feat: routes created

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home } from "../screens/Home";
+import { New } from "../screens/New";
+import { Habit } from "../screens/Habit";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="Home" component={Home} />
+      <Screen name="New" component={New} />
+      <Screen name="Habit" component={Habit} />
+    </Navigator>
+  );
+}
