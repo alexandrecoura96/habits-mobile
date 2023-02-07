112fa2691f77dfbfc88eba6839d178b763251960
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-07 17:02:01 -0300

    chore:  Header: improvements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,16 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 import Logo from "../../assets/logo.svg";
+import { useNavigation } from "@react-navigation/native";
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row item-center justify-between">
       <Logo />
       <TouchableOpacity
+        onPress={() => navigate("New")}
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
       >
